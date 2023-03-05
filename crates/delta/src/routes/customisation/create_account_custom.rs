e9c80d4b74a9451683d29f41ee41baade1a426b4
taotao2008 <huangjuntao1987@126.com>
2023-03-05 12:30:22 +0800

    提交与后台计费模块打通

--- crates/delta/src/routes/customisation/create_account_custom.rs
+++ crates/delta/src/routes/customisation/create_account_custom.rs
@@ -32,7 +32,7 @@ pub struct DataCreateAccount {
 ///
 /// Create a new account.
 #[openapi(tag = "Account")]
-#[post("/", data = "<data>")]
+#[post("/create", data = "<data>", rank=0)]
 pub async fn create_account_custom(
     authifier: &State<Authifier>,
     data: Json<DataCreateAccount>,
