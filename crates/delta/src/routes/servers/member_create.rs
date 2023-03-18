f142e8be5d5386e94f88f514d4c80424b06feb7f
taotao2008 <huangjuntao1987@126.com>
2023-03-18 19:54:20 +0800

    提交创建用户时自动将用户加入内置联邦

---
+++ crates/delta/src/routes/servers/member_create.rs
@@ -0,0 +1,45 @@
+use revolt_quark::{
+    models::{server_member::RemovalIntention, User},
+    perms, Db, EmptyResponse, Error, Permission, Ref, Result,
+};
+
+/// # Create Member
+///
+/// Removes a member from the server.
+#[openapi(tag = "Server Members")]
+#[post("/<target>/members/<member>")]
+pub async fn req(db: &Db, user: User, target: Ref, member: Ref) -> Result<EmptyResponse> {
+    let server = target.as_server(db).await?;
+
+    if member.id == user.id {
+        return Err(Error::CannotRemoveYourself);
+    }
+
+    if member.id == server.owner {
+        return Err(Error::InvalidOperation);
+    }
+
+
+
+    let member = member.as_member(db, &server.id).await?;
+
+
+    let member_create = Member {
+        id: MemberCompositeKey {
+            server: server.id.clone(),
+            user: member.id.clone(),
+        },
+        joined_at: Timestamp::now_utc(),
+        nickname: None,
+        avatar: None,
+        roles: vec![],
+        timeout: None,
+    };
+
+
+    server
+        .create_member(db, member_create, None)
+        .await
+        .map(|_| EmptyResponse)
+
+}
