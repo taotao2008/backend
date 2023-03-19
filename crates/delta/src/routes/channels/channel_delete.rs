c6bfd21aeb9062ee8de54341cc21f471f87f8691
taotao2008 <huangjuntao1987@126.com>
2023-03-19 14:46:20 +0800

    内置机器人DM不能删除

--- crates/delta/src/routes/channels/channel_delete.rs
+++ crates/delta/src/routes/channels/channel_delete.rs
@@ -23,23 +23,31 @@ pub async fn req(
     target: Ref,
     options: OptionsChannelDelete,
 ) -> Result<EmptyResponse> {
+
     let mut channel = target.as_channel(db).await?;
     let mut perms = perms(&user).channel(&channel);
     perms.throw_permission(db, Permission::ViewChannel).await?;
 
     match &channel {
         Channel::SavedMessages { .. } => Err(Error::NoEffect),
-        Channel::DirectMessage { .. } => channel
-            .update(
-                db,
-                PartialChannel {
-                    active: Some(false),
-                    ..Default::default()
-                },
-                vec![],
-            )
-            .await
-            .map(|_| EmptyResponse),
+        Channel::DirectMessage { .. } => {
+            let mut target = target.as_user(db).await?;
+            //taotao 内置机器人的DM不能删除
+            if user.bot.is_some() || target.bot.is_some() {
+                return Err(Error::IsBot);
+            }
+            channel
+                .update(
+                    db,
+                    PartialChannel {
+                        active: Some(false),
+                        ..Default::default()
+                    },
+                    vec![],
+                )
+                .await
+                .map(|_| EmptyResponse)
+        },
         Channel::Group { .. } => channel
             .remove_user_from_group(
                 db,
