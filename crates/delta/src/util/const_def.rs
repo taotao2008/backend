e8194a127418672d559b0854b2f3e9c2088279f7
taotao2008 <huangjuntao1987@126.com>
2023-03-21 20:19:05 +0800

    管理员才能创建Bot

--- crates/delta/src/util/const_def.rs
+++ crates/delta/src/util/const_def.rs
@@ -1,9 +1,9 @@
 
 // 多元世界常量定义
 // 定义内置联邦1
-pub static DEFAULT_SERVER_ID_1: &'static str = "01GVT45JCEKSAE6YRKA9NQWF3S";
+pub static DEFAULT_SERVER_ID_1: &'static str = "01GW21PDCKBYBH3HM0RG3GV2D1";
 // 定义内置机器人1
-pub static DEFAULT_BOT_ID_1: &'static str = "01GVR2VZCS7FXJYT2E75WV6FTW";
+pub static DEFAULT_BOT_ID_1: &'static str = "01GW21K5B3Q9B5HHHNEYY4AKYP";
 // 定义管理员
 pub static DEFAULT_ADMIN_ID_1: &'static str = "01GW217Y32AB5R00PB23Y1JCJA";
 // 定义后台计费系统API地址， authifier::create_account 还有一处定义
