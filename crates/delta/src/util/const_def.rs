76f9823bb0ad97e35d739246f46fda5851808541
taotao2008 <huangjuntao1987@126.com>
2023-03-21 20:13:23 +0800

    管理员才能创建Bot

--- crates/delta/src/util/const_def.rs
+++ crates/delta/src/util/const_def.rs
@@ -5,6 +5,6 @@ pub static DEFAULT_SERVER_ID_1: &'static str = "01GVT45JCEKSAE6YRKA9NQWF3S";
 // 定义内置机器人1
 pub static DEFAULT_BOT_ID_1: &'static str = "01GVR2VZCS7FXJYT2E75WV6FTW";
 // 定义管理员
-pub static DEFAULT_ADMIN_ID_1: &'static str = "01GVR2SDQ6J2W3XWWCPGK38C5G";
+pub static DEFAULT_ADMIN_ID_1: &'static str = "01GW217Y32AB5R00PB23Y1JCJA";
 // 定义后台计费系统API地址， authifier::create_account 还有一处定义
 pub static ADMIN_URL: &'static str = "http://bk.securechat.cn:8085";
