fdf8577e0e488c89e1b5bda1129b983d17f83724
taotao2008 <huangjuntao1987@126.com>
2023-04-04 14:55:35 +0800

    进行默认用户设置和添加机器人2

--- crates/delta/src/routes/onboard/complete.rs
+++ crates/delta/src/routes/onboard/complete.rs
@@ -141,7 +141,7 @@ pub async fn req(
 
 
     let mut settings_data: UserSettings = HashMap::new();
-    settings_data.insert("appearance", data_item );
+    settings_data.insert(&"appearance",data_item );
 
     info!("settings_data=");
     //info!("{}", settings_data.get(session.user_id.clone()).0);
