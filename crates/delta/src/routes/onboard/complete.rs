4fd8d30e1d49211599da1d6dd8d05c5b5ca2dfad
taotao2008 <huangjuntao1987@126.com>
2023-04-04 14:11:36 +0800

    进行默认用户设置和添加机器人2

--- crates/delta/src/routes/onboard/complete.rs
+++ crates/delta/src/routes/onboard/complete.rs
@@ -1,15 +1,22 @@
 use crate::util::regex::RE_USERNAME;
 use revolt_quark::{
-    authifier::models::Session, models::User, models::Channel, Database, EmptyResponse, Error, Result,
+    authifier::models::Session, models::User, models::UserSettings, models::Channel, Database, EmptyResponse, Error, Result,
 };
+
 
 use rocket::{serde::json::Json, State};
 use serde::{Deserialize, Serialize};
 use validator::Validate;
 use ulid::Ulid;
+use std::collections::HashMap;
+use revolt_quark::r#impl::UserSettingsImpl;
+use chrono::Utc;
 
 use crate::util::const_def::DEFAULT_SERVER_ID_1;
 use crate::util::const_def::DEFAULT_BOT_ID_1;
+use crate::util::const_def::DEFAULT_BOT_ID_2;
+
+type Data = HashMap<String, String>;
 
 /// # New User Data
 #[derive(Validate, Serialize, Deserialize, JsonSchema)]
@@ -18,6 +25,7 @@ pub struct DataOnboard {
     #[validate(length(min = 2, max = 32), regex = "RE_USERNAME")]
     username: String,
 }
+
 
 /// # Complete Onboarding
 ///
@@ -49,7 +57,7 @@ pub async fn req(
 
 
 
-    //taotao 自动添加内置机器人1
+    //taotao 自动添加内置机器人1-chatgpt
     let mut user_request_1 = db.fetch_user(&session.user_id.clone()).await?;
     let mut user_bot_1 = db.fetch_user(&DEFAULT_BOT_ID_1.to_owned()).await?;
     //taotao 步骤一：机器人发出添加好友请求
@@ -61,27 +69,75 @@ pub async fn req(
     let mut user_bot_accept_1 = db.fetch_user(&DEFAULT_BOT_ID_1.to_owned()).await?;
     user_accept_1.add_friend(db, &mut user_bot_accept_1).await?;
     Json(user_bot_accept_1.with_auto_perspective(db, &user_accept_1).await);
+    //taotao 自动添加内置机器人1-chatgpt-结束
 
-    //taotao 创建内置DM
 
+
+    //taotao 自动添加内置机器人2-Dalle-E
+    let mut user_request_2 = db.fetch_user(&session.user_id.clone()).await?;
+    let mut user_bot_2 = db.fetch_user(&DEFAULT_BOT_ID_2.to_owned()).await?;
+    //taotao 步骤一：机器人发出添加好友请求
+    user_bot_2.add_friend(db, &mut user_request_2).await?;
+    Json(user_request_2.with_auto_perspective(db, &user_bot_2).await);
+
+    //taotao 步骤二：好友同意机器人
+    let mut user_accept_2 = db.fetch_user(&session.user_id.clone()).await?;
+    let mut user_bot_accept_2 = db.fetch_user(&DEFAULT_BOT_ID_2.to_owned()).await?;
+    user_accept_2.add_friend(db, &mut user_bot_accept_2).await?;
+    Json(user_bot_accept_2.with_auto_perspective(db, &user_accept_2).await);
+    //taotao 自动添加内置机器人2-Dalle-E-结束
+
+    //taotao 创建内置DM-机器人1-chatgpt
     // Otherwise try to find or create a DM.
-    if let Ok(channel) = db.find_direct_message_channel(&session.user_id.clone(), &DEFAULT_BOT_ID_1.to_owned()).await {
-        Json(channel);
+    if let Ok(channel_1) = db.find_direct_message_channel(&session.user_id.clone(), &DEFAULT_BOT_ID_1.to_owned()).await {
+        Json(channel_1);
     } else {
-        let new_channel = Channel::DirectMessage {
+        let new_channel_1 = Channel::DirectMessage {
             id: Ulid::new().to_string(),
             active: true,
             recipients: vec![session.user_id.clone(), DEFAULT_BOT_ID_1.to_owned()],
             last_message_id: None,
         };
 
-        new_channel.create(db).await?;
-        Json(new_channel);
+        new_channel_1.create(db).await?;
+        Json(new_channel_1);
     }
+    //taotao 创建内置DM-机器人1-chatgpt-结束
 
 
-    let server = db.fetch_server(&DEFAULT_SERVER_ID_1.to_owned()).await?;
+    //taotao 创建内置DM-机器人2-dalle-e
+    // Otherwise try to find or create a DM.
+    if let Ok(channel_2) = db.find_direct_message_channel(&session.user_id.clone(), &DEFAULT_BOT_ID_2.to_owned()).await {
+        Json(channel_2);
+    } else {
+        let new_channel_2 = Channel::DirectMessage {
+            id: Ulid::new().to_string(),
+            active: true,
+            recipients: vec![session.user_id.clone(), DEFAULT_BOT_ID_2.to_owned()],
+            last_message_id: None,
+        };
+
+        new_channel_2.create(db).await?;
+        Json(new_channel_2);
+    }
+    //taotao 创建内置DM-机器人2-dalle-e-结束
+
+    //进行默认用户设置
+    let data_json = json!({
+        "appearance": { "appearance:emoji":"mutant", "appearance:show_send_button":true }
+    });
+
+    let current_time = Utc::now().timestamp_millis();
+
+    let mut settings_data: UserSettings = HashMap::new();
+    settings_data.insert(session.user_id.clone(), (current_time, data_json.to_string()));
+
+    db.set_user_settings(&session.user_id.clone(), &settings_data ).await?;
+    //进行默认用户设置-结束
+
+
     //taotao 自动加入内置联邦
+    let server = db.fetch_server(&DEFAULT_SERVER_ID_1.to_owned()).await?;
     server
         .create_member(db, user, None)
         .await
