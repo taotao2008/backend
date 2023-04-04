a4c5e6a16a1e75bce648eedf6f260b4ac3794788
taotao2008 <huangjuntao1987@126.com>
2023-04-04 14:12:01 +0800

    进行默认用户设置和添加机器人2

--- crates/delta/src/util/const_def.rs
+++ crates/delta/src/util/const_def.rs
@@ -2,8 +2,10 @@
 // 多元世界常量定义
 // 定义内置联邦1
 pub static DEFAULT_SERVER_ID_1: &'static str = "01GW21PDCKBYBH3HM0RG3GV2D1";
-// 定义内置机器人1
+// 定义内置机器人1-ChatGPT
 pub static DEFAULT_BOT_ID_1: &'static str = "01GW21K5B3Q9B5HHHNEYY4AKYP";
+// 定义内置机器人2-DALL-E
+pub static DEFAULT_BOT_ID_2: &'static str = "01GW47PSTNTDXKK9TB933NBGKA";
 // 定义管理员
 pub static DEFAULT_ADMIN_ID_1: &'static str = "01GW217Y32AB5R00PB23Y1JCJA";
 // 定义后台计费系统API地址， authifier::create_account 还有一处定义
