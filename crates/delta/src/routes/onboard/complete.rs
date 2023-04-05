89da0ddbf458fb3c3a3eb50f19e2b457f1309962
taotao2008 <huangjuntao1987@126.com>
2023-04-05 16:07:28 +0800

    添加默认内置AiZen客服

--- crates/delta/src/routes/onboard/complete.rs
+++ crates/delta/src/routes/onboard/complete.rs
@@ -15,6 +15,7 @@ use chrono::Utc;
 use crate::util::const_def::DEFAULT_SERVER_ID_1;
 use crate::util::const_def::DEFAULT_BOT_ID_1;
 use crate::util::const_def::DEFAULT_BOT_ID_2;
+use crate::util::const_def::DEFAULT_HELP_ID_3;
 
 type Data = HashMap<String, String>;
 
@@ -121,6 +122,23 @@ pub async fn req(
         Json(new_channel_2);
     }
     //taotao 创建内置DM-机器人2-dalle-e-结束
+
+    //taotao 创建内置DM-AiZen客服
+    // Otherwise try to find or create a DM.
+    if let Ok(channel_3) = db.find_direct_message_channel(&session.user_id.clone(), &DEFAULT_HELP_ID_3.to_owned()).await {
+        Json(channel_3);
+    } else {
+        let new_channel_3 = Channel::DirectMessage {
+            id: Ulid::new().to_string(),
+            active: true,
+            recipients: vec![session.user_id.clone(), DEFAULT_HELP_ID_3.to_owned()],
+            last_message_id: None,
+        };
+
+        new_channel_3.create(db).await?;
+        Json(new_channel_3);
+    }
+    //taotao 创建内置DM-AiZen客服-结束
 
     //进行默认用户设置
     let current_time = Utc::now().timestamp_millis();
--- crates/delta/src/util/const_def.rs
+++ crates/delta/src/util/const_def.rs
@@ -6,6 +6,9 @@ pub static DEFAULT_SERVER_ID_1: &'static str = "01GW21PDCKBYBH3HM0RG3GV2D1";
 pub static DEFAULT_BOT_ID_1: &'static str = "01GW21K5B3Q9B5HHHNEYY4AKYP";
 // 定义内置机器人2-DALL-E
 pub static DEFAULT_BOT_ID_2: &'static str = "01GW47PSTNTDXKK9TB933NBGKA";
+// 定义内置AiZen客服一
+pub static DEFAULT_HELP_ID_3: &'static str = "01GW41YJ06Z2NV2F743C1VCZ8M";
+
 // 定义管理员
 pub static DEFAULT_ADMIN_ID_1: &'static str = "01GW217Y32AB5R00PB23Y1JCJA";
 // 定义后台计费系统API地址， authifier::create_account 还有一处定义
