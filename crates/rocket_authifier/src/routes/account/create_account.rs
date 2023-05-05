e7d10d91c1fcfd5bb42fad2951220b04017e5a68
taotao2008 <huangjuntao1987@126.com>
2023-05-05 13:52:17 +0800

    修改为手机号，不验证Email

--- crates/rocket_authifier/src/routes/account/create_account.rs
+++ crates/rocket_authifier/src/routes/account/create_account.rs
@@ -45,35 +45,35 @@ pub async fn create_account(
     let data = data.into_inner();
 
     // Check Captcha token
-    //authifier.config.captcha.check(data.captcha).await?;
+    authifier.config.captcha.check(data.captcha).await?;
 
     // Validate the request
-    //shield.email = Some(data.email.to_string());
-    //authifier.config.shield.validate(shield).await?;
+    shield.email = Some(data.email.to_string());
+    authifier.config.shield.validate(shield).await?;
 
     // Make sure email is valid and not blocked
-    //authifier
-     //   .config
-     //   .email_block_list
-     //   .validate_email(&data.email)?;
+    authifier
+        .config
+        //.email_block_list
+        .validate_email(&data.email)?;
 
     // Ensure password is safe to use
-    //authifier
-      //  .config
-       // .password_scanning
-       // .assert_safe(&data.password)
-       // .await?;
+    authifier
+        .config
+        .password_scanning
+        .assert_safe(&data.password)
+        .await?;
 
     // If required, fetch valid invite
-    //let invite = if authifier.config.invite_only {
-    //    if let Some(invite) = data.invite {
-    //        Some(authifier.database.find_invite(&invite).await?)
-    //    } else {
-    //        return Err(Error::MissingInvite);
-    //    }
-   // } else {
-     //   None
-    //};
+    let invite = if authifier.config.invite_only {
+        if let Some(invite) = data.invite {
+            Some(authifier.database.find_invite(&invite).await?)
+        } else {
+            return Err(Error::MissingInvite);
+        }
+    } else {
+        None
+    };
 
 
     let email = &data.email;
@@ -83,12 +83,12 @@ pub async fn create_account(
     let account = Account::new(authifier, email.to_string(), password.to_string(), true).await?;
 
     // Use up the invite
-    //if let Some(mut invite) = invite {
-    //    invite.claimed_by = Some(account.id);
-    //    invite.used = true;
+    if let Some(mut invite) = invite {
+        invite.claimed_by = Some(account.id);
+        invite.used = true;
 
-    //    authifier.database.save_invite(&invite).await?;
-   // }
+        authifier.database.save_invite(&invite).await?;
+    }
 
     //同步创建后台账号
     //if let Ok(res) = create_account_external(password.to_owned(), email.to_owned()).await {
